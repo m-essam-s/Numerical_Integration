0740b3dde43f595578a71eb02e03972a4c65cfa3
m-essam-s <sir.m.essam.g@gmail.com>
2023-11-18 15:36:07 +0200

    Code Updates

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import numpy as np
 from math import *
 
 class Numerical_Integration:
-
     def __init__(self, F, LB, UB):
         self.Function = F
         self.LowerBound = LB
@@ -15,7 +14,7 @@ class Numerical_Integration:
         return eval(self.Function, {'x': x, 'np': np})
         
     def Integrat_0(self): #---> Exact Solution
-        val1, val2,Times  = 0, 0, 100000
+        val1, val2,Times  = 0, 0, 10000
         for n in range(1, Times + 1):
             val1 += self.f(self.LowerBound + ((n - 1/2) * ((self.StepSize) / Times)))
         val2 = ((self.StepSize) / Times) * val1
@@ -46,39 +45,29 @@ class Numerical_Integration:
         I4=((1/8)*((self.UpperBound)-(7*(self.StepSize/10)))*(self.f(7*(self.StepSize/10))+3*self.f(9*(self.StepSize/10))+3*self.f(8*(self.StepSize/10))+self.f((self.UpperBound))))
         T=I1+I2+I3+I4
         return T
+    def err (self, x):    #---> Error Percentage
+        T = str(abs(((float(str(self.Integrat_0()))-float(x))/float(str(self.Integrat_0())))*100))
+        return T
     
     def __str__(self):
-        I0=str(Numerical_Integration.Integrat_0(self))
-        I1=str(Numerical_Integration.Integrat_1(self))
-        T1=str(abs(((float(I0)-float(I1))/float(I0))*100))
-        I2=str(Numerical_Integration.Integrat_2(self))
-        T2=str(abs(((float(I0)-float(I2))/float(I0))*100))
-        I3=str(Numerical_Integration.Integrat_3(self))
-        T3=str(abs(((float(I0)-float(I3))/float(I0))*100))
-        I4=str(Numerical_Integration.Integrat_4(self))
-        T4=str(abs(((float(I0)-float(I4))/float(I0))*100))
-        I5=str(Numerical_Integration.Integrat_5(self))
-        T5=str(abs(((float(I0)-float(I5))/float(I0))*100))
-        
-        Result={"True Value                  |": I0,
-                "Trapezoidal Rule            |": I1,
-                "Simpson's 1/3 Rule          |": I2,
-                "Simpson's 3/8 Rule          |": I3,
-                "Compination 1/3 & 3/8 Rule  |": I4,
-                "1/3 & 3/8 & 1/3 & 3/8 Rule  |": I5}
+        Result={"True Value                  |": str(self.Integrat_0()),
+                "Trapezoidal Rule            |": str(self.Integrat_1()),
+                "Simpson's 1/3 Rule          |": str(self.Integrat_2()),
+                "Simpson's 3/8 Rule          |": str(self.Integrat_3()),
+                "Compination 1/3 & 3/8 Rule  |": str(self.Integrat_4()),
+                "1/3 & 3/8 & 1/3 & 3/8 Rule  |": str(self.Integrat_5())}
         T_V=   {"True Value                  |": f"0 %",
-                "Trapezoidal Rule            |": f"{T1} %",
-                "Simpson's 1/3 Rule          |": f"{T2} %",
-                "Simpson's 3/8 Rule          |": f"{T3} %",
-                "Compination 1/3 & 3/8 Rule  |": f"{T4} %",
-                "1/3 & 3/8 & 1/3 & 3/8 Rule  |": f"{T5} %"}
-        Table = pd.DataFrame({'Result            ': Result,
-                                '|εₜ|                 ': T_V})
+                "Trapezoidal Rule            |": f"{self.err(self.Integrat_1())} %",
+                "Simpson's 1/3 Rule          |": f"{self.err(self.Integrat_2())} %",
+                "Simpson's 3/8 Rule          |": f"{self.err(self.Integrat_3())} %",
+                "Compination 1/3 & 3/8 Rule  |": f"{self.err(self.Integrat_4())} %",
+                "1/3 & 3/8 & 1/3 & 3/8 Rule  |": f"{self.err(self.Integrat_5())} %"}
+        Table = pd.DataFrame({'Results ': Result,'|εₜ| ': T_V})
     
         return f"{Table}"
     
     def Graph(self):
-        xAxisPoints=np.linspace(floor(self.LowerBound)-1,ceil(self.UpperBound)+1,100000)
+        xAxisPoints=np.linspace(floor(self.LowerBound),ceil(self.UpperBound),10000)
         yAxisPoints=self.f(xAxisPoints)
         ax = plt.gca()
         section = np.arange(self.LowerBound,self.UpperBound, 1/1000.)
